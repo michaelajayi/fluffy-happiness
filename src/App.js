1af25da9d6c276b622c8bde2745e4de88e195e1e
MICHAEL AJAYI O <ajayimichael@hotmail.com>
2022-12-12 19:24:01 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
+import React from "react";
+import codeSpaces from "./assets/img/codeblock.png";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <h1 className='heading-text'>Hello there</h1>
+      <div className='img-block'>
+        <img src={codeSpaces} alt='code-spaces' />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
